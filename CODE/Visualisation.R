022e23c23e092f730550a2373a858e6503cd2501
Vittoria <vittoria.piatti@yahoo.it>
2023-08-17 10:52:53 +0200

    DataViz update

--- CODE/Visualisation.R
+++ CODE/Visualisation.R
@@ -15,7 +15,9 @@ households <- c('totale famiglie',
                 'almeno un anziano',
                 'monogenitore', 
                 '1 figlio minore','almeno un figlio minore')
-
+eng_households <-c("1 person household 65+","Household with elderly", "Italians-only household",
+                   "1 person household 35-64", "All households", "Household with 1 minor",
+                   "Single- parent", "Household with minors", "Foreigners-only household")
 pbht_ass<-povbyhouseholdtype16_21%>%
   filter(TIPO_DATO8=='INCID_POVASS_FAM')%>%
   select(c(TIME, `Tipologia familiare`,ITTER107,Value))%>% 
@@ -26,9 +28,9 @@ pbhtasschart <- povbyhouseholdtype16_21%>%
   select(c(TIME, `Tipologia familiare`,ITTER107,Value))%>% 
   filter(`Tipologia familiare` %in% households)%>% 
   mutate(ITTER107 = as.character(ITTER107))%>%
-  mutate(ITTER107 = replace(ITTER107, ITTER107 == "ITCD", 'N'))%>%
-  mutate(ITTER107 = replace(ITTER107, ITTER107 == "ITE", 'C'))%>%
-  mutate(ITTER107 = replace(ITTER107, ITTER107 == "ITFG", 'S'))
+  mutate(ITTER107 = replace(ITTER107, ITTER107 == "ITCD", 'North'))%>%
+  mutate(ITTER107 = replace(ITTER107, ITTER107 == "ITE", 'Centre'))%>%
+  mutate(ITTER107 = replace(ITTER107, ITTER107 == "ITFG", 'South and Islands'))
 
 
 pbht_ass<- pbht_ass%>% 
@@ -54,6 +56,10 @@ pbht_rel<-povbyhouseholdtype16_21%>%
 
 
 #####VIZ####
+
+#themecolours = c("#7D9B9F","#3A1625","#D09C32","#495751","#EDEEEB")
+themecolours = c("#495751","#3A1625","#D09C32")
+
 totbyarea <- povbyhouseholdtype16_21%>%
   filter(TIP_FAM=='HH'& ITTER107!='IT')
 gtotbyarea = ggplot(totbyarea, aes(x=TIME, y=TIPO_DATO8, size = Value, color=Territorio)) +
@@ -96,53 +102,6 @@ gfamnationality = ggplot(data=(povbyhouseholdtype16_21%>% filter((TIP_FAM=='WH_F
 gfamnationality
 
 
-#####A FIRST COMPLEX CHART#####
-
-plt <- ggplot(pbhtasschart) +
-  # Make custom panel grid
- geom_hline(
-    aes(yintercept = y), 
-    data.frame(y = c(0:3) * 1000),
-    color = "lightgrey"
-  ) + 
-
-  geom_bar(
-    aes(
-      x = `Tipologia familiare`, 
-      y = Value
-      #fill=
-    ),
-    position = "dodge2",
-    show.legend = TRUE,
-    alpha = .9
-  ) 
-  
-  # Add dots to represent the mean gain
-  geom_point(
-    aes(
-      x = reorder(str_wrap(region, 5),sum_length),
-      y = mean_gain
-    ),
-    size = 3,
-    color = "gray12"
-  ) +
-  
-  # Lollipop shaft for mean gain per region
-  geom_segment(
-    aes(
-      x = reorder(str_wrap(region, 5), sum_length),
-      y = 0,
-      xend = reorder(str_wrap(region, 5), sum_length),
-      yend = 3000
-    ),
-    linetype = "dashed"
-  ) + 
-  
-  # Make it circular!
-  coord_polar()
-
-plt
-
 ######ABSOLUTE POVERTY BY FAMILY TYPE - DIFFERENCIATED BY AREA (N-C-S) - Change 2021/2017####  
 layer1 <- pbhtasschart%>%filter(ITTER107!='IT')%>%filter(TIME ==2021)
 layer2 <- pbhtasschart%>%filter(ITTER107!='IT')%>%filter(TIME ==2017)
@@ -157,26 +116,27 @@ crt = ggplot()+
   theme_minimal_grid(font_size = 8)+
   geom_col(data= layer1,
            aes(x= reorder(str_wrap(`Tipologia familiare`, 7), Value), y=Value, fill=ITTER107),
-           show.legend = FALSE,position=position_dodge(5))+
+           show.legend = FALSE,position=position_dodge(5), alpha=.8)+
+  scale_fill_manual(values=themecolours)+
   facet_wrap(ITTER107~ .)+
   geom_col(data= layer2,
            aes(x= reorder(str_wrap(`Tipologia familiare`, 7), Value), y=Value),
            linetype="dashed",
-           size=0.75,
-           color="#375F53", 
+           linewidth =0.75,
+           color="black", 
            fill="transparent",
            position=position_dodge(5))+
   geom_point((data= avglayer1),
-            mapping=aes(x= reorder(str_wrap(`Tipologia familiare`, 7), Value),y=Value,size = 0.1),
-            color="#000000",show.legend = FALSE)+
+            mapping=aes(x= reorder(str_wrap(`Tipologia familiare`, 7), Value),y=Value),
+            shape=21, fill="#7D9B9F",color="black", size=1.7,show.legend = FALSE)+
   geom_point((data= avglayer2),
-            mapping=aes(x= reorder(str_wrap(`Tipologia familiare`, 7), Value), y=Value,
-            size = 0.1,alpha=0.35),show.legend = FALSE) + 
+            mapping=aes(x= reorder(str_wrap(`Tipologia familiare`, 7), Value), y=Value),
+            shape=23, fill="#7D9B9F", color="white",size=0.9,show.legend = FALSE) + 
   scale_y_continuous(
     limits = c(-20, 45),
     expand = c(0, 0),
     breaks = c(0, 5,10,15,20,25,30,35,40,45))+
-  scale_x_discrete(breaks = waiver())+
+  scale_x_discrete(breaks = waiver(),labels=str_wrap(eng_households, 7))+
   coord_polar()
 crt  
   
@@ -250,4 +210,19 @@ crt+ annotate(
     color = "gray12",
     size = 2,
     check_overlap = TRUE
-  )
+  )+
+  #labs(
+   # title = "\nAbsolute Poverty Incidence by Family Type and Area - 2021",
+    #subtitle = paste(
+     # "\nFor the three macroareas in Italy, some family types and the associated incidence of absolute poverty is shown.",
+      #"Dots represent the national average for the group while diamonds represent the national average in 2017.",
+      #sep = "\n"
+    #))+
+    #caption = "\n\nData Visualisation by Vittoria Piatti") 
+  theme(
+    #text = element_text(color = "gray12", family = "Times New Roman"),
+    panel.spacing.x=unit(0.5, "cm"),
+    axis.title.y = element_blank(),
+    axis.title.x = element_blank(),
+    axis.ticks.y = element_blank(),
+    axis.text.y = element_blank())
